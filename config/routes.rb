9bc5576adb619e7e254889b14ca40cfcb25af76b
VuThiHien94 <khoanglang2368@gmail.com>
2017-04-17 17:25:19 +0700

    login logout use devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	root "static_pages#home"
+	devise_for :users, path: "", path_names: {
+    sign_in: "login", sign_out: "logout",
+    password: "secret", confirmation: "verification",
+    unlock: "unblock", registration: "register", sign_up: "" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
